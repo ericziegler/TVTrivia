1bf5e5e7b059470eee56d6dae0de47b19d071579
Eric <eric@zigabytes.com>
2020-03-22 23:02:23 -0400

    EZ: Initial project commit.

---
+++ TVTrivia/TVTrivia/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  TVTrivia
+//
+//  Created by Eric Ziegler on 3/22/20.
+//  Copyright © 2020 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+}
+
---
+++ TVTrivia/TVTrivia/MainController.swift
@@ -0,0 +1,20 @@
+//
+//  MainController.swift
+//  TVTrivia
+//
+//  Created by Eric Ziegler on 3/22/20.
+//  Copyright © 2020 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
